e7b87e39508900c1484415f1a996f5349f4f7034
10322057 <debora.parisi@mail.polimi.it>
2015-03-31 16:05:22 +0200

    mio file

---
+++ cmake_example1/BUILD/CMakeFiles/hellomake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellomake.dir/hellomake.cpp.o"
+  "hellomake.pdb"
+  "hellomake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellomake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
